384637e3ce6df609d57fece856263b6d5e68e421
GEORGI PEHLIVANOV <georgipehlivanov@GEORGIs-Mac-mini.local>
2025-08-29 18:19:39 +0300

    Reverted all changes back to version ea596677ca8e073c640bfe035fd3b4dcde9efc0c

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,13 @@ import { defineConfig } from "vite";
 import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    dyadComponentTagger(), 
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects',
-          dest: '.'
-        }
-      ]
-    })
-  ],
+  plugins: [dyadComponentTagger(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
